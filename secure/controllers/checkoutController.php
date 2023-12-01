7f5bc5305331fea4aa9d92f40ab512c56ecc1eae
AxGudrun <axel.gudehn@gmail.com>
2023-12-01 16:25:27 +0100

    Session hijack prevention + crypto wallet stuffs

--- secure/controllers/checkoutController.php
+++ secure/controllers/checkoutController.php
@@ -2,7 +2,8 @@
 $arr = array();
 $exit_code = null;
 $res = exec("./cli server getwalletaddress -apiport 8080", $arr, $exit_code);
-// /Users/emanuelsjovall/lth/websec/simpleBlockchain/cli server getwalletaddress -apiport 8080
+// Emanuels ---/Users/emanuelsjovall/lth/websec/simpleBlockchain/cli server getwalletaddress -apiport 8080
+// Axels    ---/Applications/XAMPP/xamppfiles/htdocs/SimpleBlockchain/./cli server getwalletaddress -apiport 8080
 
 print_r($arr);
 echo "</br>";
--- secure/controllers/logInController.php
+++ secure/controllers/logInController.php
@@ -15,8 +15,9 @@ if (mysqli_num_rows($resultGetUser) === 0){
 $rowUser = mysqli_fetch_assoc($resultGetUser);
 if ($rowUser['password'] === hash('sha256', $pass . $rowUser['salt'])){
     session_start();
+    $secureSessionId = bin2hex(random_bytes(64));
     if (isset($_POST['keepLoggedIn']) && $_POST['keepLoggedIn'] == 'on') {
-        setcookie('username', $username, time() + (86400 * 30), "/"); 
+        setcookie('secure_session_id', $secureSessionId, time() + (86400 * 30), "/"); 
     }
     $_SESSION['username'] = $username;
     header("Location: getProducts.php");
