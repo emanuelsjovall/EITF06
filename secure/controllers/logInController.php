edbb41c6fdb9d1133818119c722df92c3a001228
Emanuel <emanuel.sjovall@gmail.com>
2023-12-05 10:35:13 +0100

    using hash_equals instead of ===, no timing attack

--- secure/controllers/logInController.php
+++ secure/controllers/logInController.php
@@ -16,7 +16,7 @@ if (mysqli_num_rows($resultGetUser) === 0){
 }
 
 $rowUser = mysqli_fetch_assoc($resultGetUser);
-if ($rowUser['password'] === hash('sha256', $pass . $rowUser['salt'])){
+if (hash_equals($rowUser['password'], hash('sha256', $pass . $rowUser['salt']))){
     session_start();
     $secureSessionId = bin2hex(random_bytes(64));
     if (isset($_POST['keepLoggedIn']) && $_POST['keepLoggedIn'] == 'on') {
